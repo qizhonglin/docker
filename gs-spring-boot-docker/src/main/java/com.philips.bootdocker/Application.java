1e8d2f6b6625858f5363f094c70e2311b2ac2d07
qizhonglin <qizhong.lin@philips.com>
2016-06-22 01:10:41 -0400

    add two project: jdk8-image-ex and gs-spring-boot-docker

---
+++ gs-spring-boot-docker/src/main/java/com.philips.bootdocker/Application.java
@@ -0,0 +1,24 @@
+package com.philips.bootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by qzlin on 6/21/16.
+ */
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
